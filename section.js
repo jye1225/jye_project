5bf1079c70afaa817a657023dc18e43dcea0bf78
yeeun <yeeun7117@sookmyung.ac.kr>
2024-04-07 06:54:33 +0900

    second commit

--- section.js
+++ section.js
@@ -26,33 +26,18 @@ $(document).ready(function () {
   });
 });
 
-// 메뉴 닫기
-// $(document).click(function (event) {
-//   if (!$(event.target).closest('.ham').length) {
-//     $(".ham").removeClass("open");
-//     $("#main").removeClass("overflow-hidden");
-//     $("#menu-container .menu-list").removeClass("active");
-//     $(".menu-list").hide();
-//   }
-// });
+const accordionToggle = document.querySelectorAll(".accordion-toggle");
 
-$(".ham").click(function (event) {
-  event.stopPropagation();
-  $(".ham").toggleClass("open");
-  $(".menu-list").toggle();
-
-  if (menuOpen) {
-    $("#main").show();
-    menuOpen = false;
-  } else {
-    $("#main").hide();
-    menuOpen = true;
-  }
+accordionToggle.forEach((toggle) => {
+  toggle.addEventListener("click", function () {
+    const submenu = this.nextElementSibling;
+    submenu.classList.toggle("open");
+  });
 });
 
 //swiper
 let swiper = new Swiper(".slide1", {
-  slidesPerView: 3,
+  slidesPerView: 2,
   spaceBetween: 30,
   loop: true,
   pagination: {
